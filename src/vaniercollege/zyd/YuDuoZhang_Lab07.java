bdac42aeb40b4d917b3a6dd10e37bdc6085530fd
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-22 10:35:38 -0400

    Initial Commit

---
+++ src/vaniercollege/zyd/YuDuoZhang_Lab07.java
@@ -0,0 +1,22 @@
+package vaniercollege.zyd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Yu Duo Zhang (2480549)
+ * 
+ */
+public class YuDuoZhang_Lab07 extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
